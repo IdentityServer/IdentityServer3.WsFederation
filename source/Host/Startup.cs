9161928e450e50412c7f2f242c33844f5125a3bb
Brock Allen <brockallen@gmail.com>
2014-10-22 20:52:04 -0400

    updates from latest myget; add host

---
+++ source/Host/Startup.cs
@@ -0,0 +1,56 @@
+﻿using Owin;
+using Host.Config;
+using Thinktecture.IdentityServer.Core.Configuration;
+using Thinktecture.IdentityServer.Host.Config;
+using Thinktecture.IdentityServer.WsFederation.Configuration;
+using Thinktecture.IdentityServer.WsFederation.Services;
+
+namespace Host
+{
+    internal class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Map("/core", coreApp =>
+            {
+                coreApp.Use(async (ctx, next) =>
+                {
+                    await next();
+                });
+                var factory = InMemoryFactory.Create(
+                    users: Users.Get(),
+                    clients: Clients.Get(),
+                    scopes: Scopes.Get());
+
+                var options = new IdentityServerOptions
+                {
+                    IssuerUri = "https://idsrv3.com",
+                    SiteName = "Thinktecture IdentityServer v3",
+                    RequireSsl = false,
+
+                    SigningCertificate = Certificate.Get(),
+                    Factory = factory,
+                    PluginConfiguration = ConfigurePlugins,
+                    AccessTokenValidationEndpoint = EndpointSettings.Enabled
+                };
+
+                coreApp.UseIdentityServer(options);
+            });
+        }
+
+        private void ConfigurePlugins(IAppBuilder pluginApp, IdentityServerOptions options)
+        {
+            var wsFedOptions = new WsFederationPluginOptions
+            {
+                IdentityServerOptions = options,
+                Factory = new WsFederationServiceFactory
+                {
+                    UserService = options.Factory.UserService,
+                    RelyingPartyService = Registration.RegisterFactory<IRelyingPartyService>(() => new InMemoryRelyingPartyService(RelyingParties.Get())),
+                }
+            };
+
+            pluginApp.UseWsFederationPlugin(wsFedOptions);
+        }
+    }
+}
