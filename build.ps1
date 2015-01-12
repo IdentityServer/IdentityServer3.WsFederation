23315cf3c84b4e909f9984cb916c572ab1d63c06
Dominick Baier <dbaier@leastprivilege.com>
2015-01-12 10:43:27 +0100

    updated build tools

--- build.ps1
+++ build.ps1
@@ -4,7 +4,6 @@ Param(
 )
 
 if(Test-Path Env:\APPVEYOR_BUILD_NUMBER) {
-	$d = Get-Date
 	$preRelease = "pre-" + [int]$Env:APPVEYOR_BUILD_NUMBER
 	Write-Host "Using APPVEYOR_BUILD_NUMBER"
 }
@@ -15,6 +14,7 @@ gci .\source -Recurse "packages.config" |% {
 	"Restoring " + $_.FullName
 	.\source\.nuget\nuget.exe i $_.FullName -o .\source\packages
 }
-Import-Module .\source\packages\psake.4.3.2\tools\psake.psm1
+
+Import-Module .\source\packages\psake.4.4.1\tools\psake.psm1
 Invoke-Psake .\default.ps1 $task -framework "4.0x64" -properties @{ buildNumber=$buildNumber; preRelease=$preRelease }
 Remove-Module psake
