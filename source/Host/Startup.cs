6db0813c962213346139a5a4f4fb4412c50bc33f
Scott Brady <scott.brady@rocksolidknowledge.com>
2016-04-26 19:35:54 +0100

    Added UseInMemoryRelyingParties extension method

--- source/Host/Startup.cs
+++ source/Host/Startup.cs
@@ -1,13 +1,9 @@
 ﻿using Host.Config;
 using IdentityServer3.Core.Configuration;
-using IdentityServer3.Core.Logging;
 using IdentityServer3.Host.Config;
 using IdentityServer3.WsFederation.Configuration;
-using IdentityServer3.WsFederation.Models;
-using IdentityServer3.WsFederation.Services;
 using Owin;
 using Serilog;
-using System.Collections.Generic;
 
 namespace Host
 {
@@ -43,10 +39,7 @@ namespace Host
         private void ConfigurePlugins(IAppBuilder pluginApp, IdentityServerOptions options)
         {
             var wsFedOptions = new WsFederationPluginOptions(options);
-
-            // data sources for in-memory services
-            wsFedOptions.Factory.Register(new Registration<IEnumerable<RelyingParty>>(RelyingParties.Get()));
-            wsFedOptions.Factory.RelyingPartyService = new Registration<IRelyingPartyService>(typeof(InMemoryRelyingPartyService));
+            wsFedOptions.Factory.UseInMemoryRelyingParties(RelyingParties.Get());
 
             pluginApp.UseWsFederationPlugin(wsFedOptions);
         }
